a5922dd5c57cd23e8b2b999a1876b37dd6633ed0
niccorinaldi <92802475+niccorinaldi@users.noreply.github.com>
2021-11-24 18:24:47 +0100

    Update P3.c

--- P3.c
+++ P3.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 int main(int argc, char *argv[]){
 
