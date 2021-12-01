8de3f60c110c75293efef95231d353dd2599cb93
niccorinaldi <92802475+niccorinaldi@users.noreply.github.com>
2021-12-01 21:39:57 +0100

    Add files via upload

---
+++ avvia.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <stdio.h>
+
+int main(int argc,char* argv[]){
+    if(argc<3){
+        printf("Inserire modalità di avvio e percorso del file\n");
+        kill(0,SIGKILL);
+    }
+    mkdir("bin",0777);
+    mkdir("log",0777);
+    if(fopen("log/alreadyInstalled.txt", "r+") == NULL) {
+        fopen("log/alreadyInstalled.txt", "w+");
+        sleep(1);
+        system("cc inputManager.c -o bin/inputManager && cc P1.c -o bin/P1 && cc P2.c -o bin/P2 && cc P3.c -o bin/P3 && cc decisionFunction.c -o bin/DF /dev/null");
+        printf("\nCompilazione completata!\n");
+    }
+    execl("bin/inputManager",argv[0],argv[1], argv[2]);
+}
