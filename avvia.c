ccdc837b86fa819f0f2f70ac84f346784bb04a40
niccorinaldi <92802475+niccorinaldi@users.noreply.github.com>
2021-12-01 22:24:01 +0100

    Update avvia.c

--- avvia.c
+++ avvia.c
@@ -12,11 +12,11 @@ int main(int argc,char* argv[]){
     }
     mkdir("bin",0777);
     mkdir("log",0777);
-    if(fopen("log/alreadyInstalled.txt", "r+") == NULL) {
-        fopen("log/alreadyInstalled.txt", "w+");
-        sleep(1);
-        system("cc InputManager.c -o bin/InputManager && cc P1.c -o bin/P1 && cc P2.c -o bin/P2 && cc P3.c -o bin/P3 && cc decisionFunction.c -o bin/DF /dev/null");
+ //   if(fopen("log/alreadyInstalled.txt", "r+") == NULL) {
+  //      fopen("log/alreadyInstalled.txt", "w+");
+  //      sleep(1);
+        system("cc InputManager.c -o bin/InputManager && cc P1.c -o bin/P1 && cc P2.c -o bin/P2 && cc P3.c -o bin/P3 && cc DecisionFunction.c -o bin/DF /dev/null");
         printf("\nCompilazione completata!\n");
-    }
+ //   }
     execl("bin/InputManager",argv[0],argv[1], argv[2], NULL);
 }
