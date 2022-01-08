c6a83e7eb5c38c203c55db8145e3916c9540fa37
niccorinaldi <92802475+niccorinaldi@users.noreply.github.com>
2022-01-08 16:19:14 +0100

    Update P2.c

--- src/P2.c
+++ src/P2.c
@@ -55,14 +55,6 @@ int decisionFunctionSocketConnection() {
   return clientFd;
 }
 
-/*int readLine (int fd, char *str) {
-  int n;
-  do {
-    n = read (fd, str, 1);
-  } while(n > 0 && *str++ != '\0');
-  return (n > 0);
-}*/
-
 ///////////////////////////////////////////////////////////////////
 //////// Metodo per l'incremento del risultato ottenuto //////////
 /////////////////////////////////////////////////////////////////
